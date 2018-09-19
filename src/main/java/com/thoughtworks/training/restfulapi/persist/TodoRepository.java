9b68339fafa3691efd774bc8e5ecdea420b1f88a
hoyhe <hoyhe@thoughtworks.com>
2018-09-19 23:34:45 +0800

    Can search between start date and end date

--- src/main/java/com/thoughtworks/training/restfulapi/persist/TodoRepository.java
+++ src/main/java/com/thoughtworks/training/restfulapi/persist/TodoRepository.java
@@ -5,13 +5,14 @@ import com.thoughtworks.training.restfulapi.model.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     Page<Todo> findAllByNameContaining(String name, Pageable pageable);
     Page<Todo> findAllByTagsIn(List<Tag> tags, Pageable pageable);
+    Page<Todo> findAllByDueDateBetween(Date startDate, Date endDate, Pageable pageable);
 }
--- src/test/java/com/thoughtworks/training/restfulapi/persist/TodoRepositoryTest.java
+++ src/test/java/com/thoughtworks/training/restfulapi/persist/TodoRepositoryTest.java
@@ -2,6 +2,7 @@ package com.thoughtworks.training.restfulapi.persist;
 
 import com.thoughtworks.training.restfulapi.model.Tag;
 import com.thoughtworks.training.restfulapi.model.Todo;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,11 +12,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashSet;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.mockito.Matchers.*;
 
@@ -31,57 +32,80 @@ public class TodoRepositoryTest {
 
     Tag tag1;
     Tag tag2;
-
-    Todo mockTodo;
+    Date startDate;
+    Date endDate;
+    Date mockDate10_8;
+    Date mockDate10_11;
+    Todo mockTodo10_8;
+    Todo mockTodo10_11;
+    private PageRequest pageable = new PageRequest(0, 10);
 
     @Before
     public void setUp() throws Exception {
         tag1 = tagRepository.saveAndFlush(new Tag(1L, "tag1"));
         tag2 = tagRepository.saveAndFlush(new Tag(2L, "tag2"));
-        mockTodo = Todo.builder()
+        startDate = new SimpleDateFormat("yyyy-MM-dd").parse("2018-10-01");
+        endDate = new SimpleDateFormat("yyyy-MM-dd").parse("2018-10-10");
+        mockDate10_8 = new SimpleDateFormat("yyyy-MM-dd").parse("2018-10-08");
+        mockDate10_11 = new SimpleDateFormat("yyyy-MM-dd").parse("2018-10-11");
+        mockTodo10_8 = Todo.builder()
                 .id(1L)
                 .name("name")
-                .dueDate(new Date())
+                .dueDate(mockDate10_8)
                 .status("todo")
                 .tags(new HashSet<>(Arrays.asList(
                         tag1, tag2
                 )))
                 .build();
-        todoRepository.saveAndFlush(mockTodo);
+        mockTodo10_11 = Todo.builder()
+                .id(11L)
+                .name("10_11")
+                .dueDate(mockDate10_11)
+                .status("todo")
+                .tags(new HashSet<>(Arrays.asList(
+                        tag1, tag2
+                )))
+                .build();
+        todoRepository.saveAndFlush(mockTodo10_8);
     }
 
     @Test
     public void shouldGetTodoByName() {
         Page<Todo> result = todoRepository.findAllByNameContaining("name", any());
-        assertThat(result.getContent().get(0), equalTo(mockTodo));
+        Assert.assertThat(result.getContent().get(0), equalTo(mockTodo10_8));
     }
 
     @Test
     public void shouldGetTodoListByOneTag() {
-        Todo mockTodo1 = mockTodo.builder()
+        Todo mockTodo1 = mockTodo10_8.builder()
                 .id(2L)
                 .name("new name")
                 .tags(new HashSet<>(Arrays.asList(tag1)))
                 .build();
         todoRepository.saveAndFlush(mockTodo1);
-        Page<Todo> result = todoRepository.findAllByTagsIn(Arrays.asList(tag1), new PageRequest(0, 5));
+        Page<Todo> result = todoRepository.findAllByTagsIn(Arrays.asList(tag1), pageable);
         //hasItem use equals and hashcode to judge two Objects match.
-        assertThat(result.getContent(), hasItems(mockTodo, mockTodo1));
+        Assert.assertThat(result.getContent(), hasItems(mockTodo10_8, mockTodo1));
 
     }
 
     @Test
     public void shouldGetTodoListByTwoTags() {
-        Todo mockTodo1 = mockTodo.builder()
+        Todo mockTodo1 = mockTodo10_8.builder()
                 .id(2L)
                 .name("new name")
                 .tags(new HashSet<>(Arrays.asList(tag1)))
                 .build();
         todoRepository.saveAndFlush(mockTodo1);
-        Page<Todo> result = todoRepository.findAllByTagsIn(Arrays.asList(tag1, tag2), new PageRequest(0, 5));
+        Page<Todo> result = todoRepository.findAllByTagsIn(Arrays.asList(tag1, tag2), pageable);
         //hasItem use equals and hashcode to judge two Objects match.
-        assertThat(result.getContent(), hasItems(mockTodo, mockTodo1));
+        Assert.assertThat(result.getContent(), hasItems(mockTodo10_8, mockTodo1));
     }
 
-
+    @Test
+    public void shouldGetTodoListBetweenDates() {
+        Page<Todo> result = todoRepository.findAllByDueDateBetween(startDate, endDate, pageable);
+        Assert.assertThat(result, hasItems(mockTodo10_8));
+        Assert.assertThat(result, not(hasItems(mockTodo10_11)));
+    }
 }
